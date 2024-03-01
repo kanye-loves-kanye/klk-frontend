2160c2b624720e88a821bd788a18ff7ba0f7b938
RobertSarsoza <sarsoza@gmail.com>
2024-03-01 14:24:48 -0800

    corrected routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Collection from './pages/Collection';
+import Collection from "./pages/Collection";
 import Home from "./pages/Home";
 import Inspiration from "./pages/Inspiration";
 import KLKEdit from "./pages/KLKEdit";
@@ -13,46 +13,29 @@ import MyCollection from "./pages/MyCollection";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import TypeOfYe from "./pages/TypeOfYe";
+import KLKNotFound from "./pages/KLKNotFound";
 
 const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/aboutus">
-            <AboutUs />
-          </Route>
-          <Route path="/collection">
-            <Collection />
-          </Route>
-          <Route path="/inspiration">
-            <Inspiration />
-          </Route>
-          <Route path="/klk-edit">
-            <KLKEdit />
-          </Route>
-          <Route path="/klk-new-item">
-            <KLKNewItem />
-          </Route>
-          <Route path="/my-collection">
-            <MyCollection />
-          </Route>
-          <Route path="/sign-in">
-            <SignIn />
-          </Route>
-          <Route path="/sign-up">
-            <SignUp />
-          </Route>
-          <Route path="/type-of-ye">
-            <TypeOfYe />
-          </Route>
-        <Footer />
-      </div>
-    </Router>
+    <div className='App'>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/inspiration' element={<Inspiration />} />
+        <Route path='/klk-edit' element={<KLKEdit />} />
+        <Route path='/klk-new-item' element={<KLKNewItem />} />
+        <Route path='/my-collection' element={<MyCollection />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/type-of-ye' element={<TypeOfYe />} />
+        <Route path='*' element={<KLKNotFound />} />
+      </Routes>
+      <Footer />
+    </div>
   );
-}
+};
 
 export default App;
