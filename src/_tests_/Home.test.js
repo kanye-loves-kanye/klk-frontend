491837750f85f5f1c76851a68b1e32e18d760e0b
RobertSarsoza <sarsoza@gmail.com>
2024-03-01 16:07:53 -0800

    Home and not found testing and basic styling, added favicon and logo

---
+++ src/_tests_/Home.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/Home'
+import { BrowserRouter } from 'react-router-dom'
+import Homepage from "../assets/Homepage.jpeg"
+
+describe("<Home />", () => {
+  it("renders an image", () => {
+    render(
+      <BrowserRouter>
+        <Home />
+      </BrowserRouter>
+    )
+    screen.logTestingPlaygroundURL()
+    const image = screen.getByRole("img")
+    expect(image).toHaveAttribute("src", Homepage)
+  })
+})
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react"
+import Homepage from "../assets/Homepage.jpeg"
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
 
+    const Home = () => {
+      return (
+        <img
+          src={Homepage}
+          alt="HiResHome"
+          className="main-content-image"
+        />
+      )
+    }
 export default Home
